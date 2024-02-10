6eeec42c01720cfe572e0a4cd27015b502359a4b
abraxas894 <artd_kda@hotmail.com>
2024-02-09 19:54:47 -0600

    Se agregaron los archivos correspondientes para el sprint imaginario

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="cuerpoEspejos">
+        <h1>Espejos</h1>
+        <h2>Fragmentos de cristalería</h2>
+      </header>
+      <section className="citaEsther">
+        <p>En muchos sentidos, podría decirse que el <cite>Malleus</cite> es un tratado sobre las debilidades del cuerpo, un desmesurado ataque contra los placeres sexuales de las llamadas brujas, que no hacen sino poner al descubierto los placeres de la mujer en general; pero más concretamente aún, el <cite>Malleus</cite> desenmascara las fantasías eróticas de los hombres que los describen. &#x2014;Esther Cohen.</p>
+      </section>
+    </div>
+  );
+}
+
+export default App;
